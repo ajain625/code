8cba7e179edcc102207184a90a36c55135c12642
Anchit Jain <ajain@hpc-gw1.hpc.swc.ucl.ac.uk>
2023-07-05 14:31:06 +0000

    resnet support with chceckpointing

--- training.py
+++ training.py
@@ -10,12 +10,16 @@ import models
 
 # Paths
 save_path = '/nfs/ghome/live/ajain/checkpoints/'
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
-model = models.LeNet5(10).to(device)
+#print(torch.cuda.get_device_name())
+#model = models.LeNet5(10).to(device)
+model = torchvision.models.resnet18().to(device)
+model.fc = nn.Linear(512, 10).to(device)
 
 # Hyperparameters
-num_epochs = 30
+num_epochs = 5
 lr = 0.01
 num_batches = 1
 
@@ -24,25 +28,28 @@ loss_metric = nn.CrossEntropyLoss()
 
 for epoch in range(num_epochs):
     for batch in range(num_batches):
-        images, labels = utils.load_cifar_batch(batch+1, normalize=True)
-        images = torch.from_numpy(images[:100, :, :, :]).to(device)
-        labels = torch.from_numpy(labels[:100]).to(device)
+        images, labels = utils.load_cifar_batch(batch+1, normalize=True, gray=False)
+        images = torch.from_numpy(images).to(device)
+        labels = torch.from_numpy(labels).to(device)
         outputs = model(images)
-        #print(outputs[:2])
-        #print(labels[:2])
         loss = loss_metric(outputs, labels)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        print('Epoch: [{}/{}], Batch: [{}/{}], Loss: {}, Accuracy: {}'.format(epoch+1, num_epochs, batch+1, num_batches, loss.item(), utils.cifar_accuracy(labels.detach().numpy(), outputs.detach().numpy())))
+        print('Epoch: [{}/{}], Batch: [{}/{}], Loss: {}, Accuracy: {}'.format(epoch+1, num_epochs, batch+1, num_batches, loss.item(), utils.cifar_accuracy(labels.detach().cpu().numpy(), outputs.detach().cpu().numpy())))
+        del images, labels, outputs
+        torch.cuda.empty_cache()
     # Test Set	
-    model.eval()
-    images, labels = utils.load_cifar_batch('test', normalize=True)
-    images = torch.from_numpy(images).to(device)
-    labels = torch.from_numpy(labels).to(device)
-    outputs = model(images)
-    loss = loss_metric(outputs, labels)
-    #print('(Test Set) Epoch: [{}/{}], Loss: {}, Accuracy: {}'.format(epoch+1, num_epochs, loss.item(), utils.cifar_accuracy(labels.detach().numpy(), outputs.detach().numpy())))
+    with torch.no_grad():
+        model.eval()
+        images, labels = utils.load_cifar_batch('test', normalize=True, gray=False)
+        images = torch.from_numpy(images).to(device)
+        labels = torch.from_numpy(labels).to(device)
+        outputs = model(images)
+        loss = loss_metric(outputs, labels)
+        print('(Test Set) Epoch: [{}/{}], Loss: {}, Accuracy: {}'.format(epoch+1, num_epochs, loss.item(), utils.cifar_accuracy(labels.detach().cpu().numpy(), outputs.detach().cpu().numpy())))
+        del images, labels, outputs
+        torch.cuda.empty_cache()
     model.train()
     # Save Model
     torch.save({'epoch': epoch, 'model_state_dict': model.state_dict(), 'optimizer_state_dict': optimizer.state_dict(), 'loss': loss}, save_path+'lenet_cifar10_'+str(epoch)+'.pth')
