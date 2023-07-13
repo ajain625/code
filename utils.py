4203b13354e09c0460b303d40989ee65eaa23cb6
Anchit Jain <jainanchit7@gmail.com>
2023-07-13 12:57:04 +0000

    random test-train split capability (TODO for joint)

--- utils.py
+++ utils.py
@@ -36,17 +36,53 @@ def normalise_images(images, gray=False, mean=np.array([0.485, 0.456, 0.406]), s
         return images.mean(axis=1, keepdims=True).astype(np.float32)
     return images.astype(np.float32)
 
-def load_cifar100_2_classes(class1, class2, gray=False, shuffle=True, seed=42):
+def load_cifar100_2_classes(class1, class2, gray=False, shuffle=True, seed=42, random_split=False, reduction=1):
     train_data = normalise_images(np.vstack([np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/train'+str(class1)+'.npy'), np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/train'+str(class2)+'.npy')]), gray=gray)
-    train_labels = np.hstack([np.zeros(len(train_data)//2, dtype=int), np.ones(len(train_data)//2, dtype=int)])
+    train_len = len(train_data)
+    train_labels = np.hstack([np.zeros(train_len//2, dtype=int), np.ones(train_len//2, dtype=int)])
     test_data = normalise_images(np.vstack([np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/test'+str(class1)+'.npy'), np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/test'+str(class2)+'.npy')]), gray=gray)
-    test_labels = np.hstack([np.zeros(len(test_data)//2, dtype=int), np.ones(len(test_data)//2, dtype=int)])
-    if shuffle:
+    test_len = len(test_data)
+    test_labels = np.hstack([np.zeros(test_len//2, dtype=int), np.ones(test_len//2, dtype=int)])
+    if random_split:
         np.random.seed(seed)
-        train_shuffle = np.random.permutation(len(train_data))
+        data = np.vstack([train_data, test_data])
+        labels = np.hstack([train_labels, test_labels])
+        shuffle = np.random.permutation(len(data))
+        data = data[shuffle]
+        labels = labels[shuffle]
+        train_data = data[:int(train_len*reduction)]
+        train_labels = labels[:int(train_len*reduction)]
+        test_data = data[-test_len:]
+        test_labels = labels[-test_len:]
+    elif shuffle:
+        np.random.seed(seed)
+        train_shuffle = np.random.permutation(train_len)
         train_data = train_data[train_shuffle]
+        train_data = train_data[:int(train_len*reduction)]
         train_labels = train_labels[train_shuffle]
+        train_labels = train_labels[:int(train_len*reduction)]
     return train_data, train_labels, test_data, test_labels
+
+def load_cifar100_4_classes_(class1a, class1b, class2a, class2b, gray=False, shuffle=True, seed=42, random_split=False, reduction=1):
+    # Think  about how to ensure same train-test as individual train-test
+    raise NotImplementedError
+    train_data_a = normalise_images(np.vstack([np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/train'+str(class1a)+'.npy'), np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/train'+str(class2a)+'.npy')]), gray=gray)
+    train_len_a = len(train_data_a)
+    train_labels_a = np.hstack([np.zeros(train_len_a//2, dtype=int), np.ones(train_len_a//2, dtype=int)])
+    train_data_b = normalise_images(np.vstack([np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/train'+str(class1b)+'.npy'), np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/train'+str(class2b)+'.npy')]), gray=gray)
+    train_len_b = len(train_data_b)
+    train_labels_b = np.hstack([np.zeros(train_len_b//2, dtype=int), np.ones(train_len_b//2, dtype=int)])
+    test_data_a = normalise_images(np.vstack([np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/test'+str(class1a)+'.npy'), np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/test'+str(class2a)+'.npy')]), gray=gray)
+    test_data_b = normalise_images(np.vstack([np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/test'+str(class1b)+'.npy'), np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/test'+str(class2b)+'.npy')]), gray=gray)
+    test_len_a = len(test_data_a)
+    test_len_b = len(test_data_b)
+    test_labels_a = np.hstack([np.zeros(test_len_a//2, dtype=int), np.ones(test_len_a//2, dtype=int)])
+    test_labels_b = np.hstack([np.zeros(test_len_b//2, dtype=int), np.ones(test_len_b//2, dtype=int)])
+    if random_split:
+        #TODO
+    elif shuffle:
+        #TODO
+    
 
 # train_data, train_labels, test_data, test_labels = load_cifar100_2_classes(1, 2)
 # plt.imshow(np.transpose(train_data[1], (1, 2, 0)))
