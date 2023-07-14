dd5dfd3e18c0496c26c03e6d8fdc3bc8fdb8aec3
Anchit Jain <jainanchit7@gmail.com>
2023-07-14 13:23:41 +0000

    new complete di function, uses new utils functions

--- di_utils.py
+++ di_utils.py
@@ -7,45 +7,33 @@ import utils
 import models
 
 
-def cifar100_individual_train(model, class1, class2, save_path = '/nfs/ghome/live/ajain/checkpoints/di_cifar100/baseline/', epochs=500, batch_size = 128, lr=0.01, momentum=0.9, weight_decay=0.0001, seed=42):
+def cifar100_individual_train(model, class1, class2, save_path = '/nfs/ghome/live/ajain/checkpoints/di_cifar100/baseline/', epochs=500, batch_size = 128, lr=0.01, momentum=0.9, weight_decay=0.0001, random_split=True, seed=42, split_order = None, fine=False):
     assert model in ['lenet', 'resnet']
     assert class1 in range(1, 21)
     assert class2 in range(1, 21)
     assert class1 != class2	
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print(device)
+    assert device == 'cuda'
     print(torch.cuda.get_device_name())
     np.random.seed(seed)
     torch.manual_seed(seed)
 
     if model == 'lenet':
         net = models.LeNet5(2)
-        train_data = utils.normalise_images(np.vstack([np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/train'+str(class1)+'.npy'), np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/train'+str(class2)+'.npy')]), gray=True)
-        train_labels = np.hstack([np.zeros(len(train_data)//2, dtype=int), np.ones(len(train_data)//2, dtype=int)])
-        test_data = utils.normalise_images(np.vstack([np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/test'+str(class1)+'.npy'), np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/test'+str(class2)+'.npy')]), gray=True)
-        test_labels = np.hstack([np.zeros(len(test_data)//2, dtype=int), np.ones(len(test_data)//2, dtype=int)])
+        train_data, train_labels, test_data, test_labels = utils.load_cifar100_2_classes(class1, class2, gray=True, shuffle=True, split_order=split_order, seed=seed, fine=fine, random_split=random_split)
         print('data and lenet loaded')
     elif model == 'resnet':
         #net = torchvision.models.resnet18()
         net = models.resnet18vw(width=32)
         net.fc = nn.Linear(256, 2)
-        train_data = utils.normalise_images(np.vstack([np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/train'+str(class1)+'.npy'), np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/train'+str(class2)+'.npy')]))
-        train_labels = np.hstack([np.zeros(len(train_data)//2, dtype=int), np.ones(len(train_data)//2, dtype=int)])
-        test_data = utils.normalise_images(np.vstack([np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/test'+str(class1)+'.npy'), np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/test'+str(class2)+'.npy')]))
-        test_labels = np.hstack([np.zeros(len(test_data)//2, dtype=int), np.ones(len(test_data)//2, dtype=int)])
+        train_data, train_labels, test_data, test_labels = utils.load_cifar100_2_classes(class1, class2, gray=False, shuffle=True, split_order=split_order, seed=seed, fine=fine, random_split=random_split)
         print('data and resnet loaded')
-    
 
     net = net.to(device)
     optimizer = torch.optim.SGD(net.parameters(), lr=lr, momentum=momentum, weight_decay=weight_decay)
     loss_fn = nn.CrossEntropyLoss()
-    print(f'class1: {class1}, class2: {class2}')
-
-    shuffle = np.random.permutation(len(train_data))
-    train_data = train_data[shuffle]
-    train_labels = train_labels[shuffle]
-
     batches = len(train_data)//batch_size
+    print(f'class1: {class1}, class2: {class2}')
 
     for epoch in range(epochs):
         for batch in range(batches-1):
@@ -66,7 +54,7 @@ def cifar100_individual_train(model, class1, class2, save_path = '/nfs/ghome/liv
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        #print('Epoch: [{}/{}], Batch: [{}/{}], Loss: {}, Accuracy: {}'.format(epoch+1, epochs, batches, batches, loss.item(), utils.cifar_accuracy(labels.detach().cpu().numpy(), outputs.detach().cpu().numpy())))
+        print('Epoch: [{}/{}], Batch: [{}/{}], Loss: {}, Accuracy: {}'.format(epoch+1, epochs, batches, batches, loss.item(), utils.cifar_accuracy(labels.detach().cpu().numpy(), outputs.detach().cpu().numpy())))
         del images, labels, outputs
         torch.cuda.empty_cache()
 
@@ -86,7 +74,7 @@ def cifar100_individual_train(model, class1, class2, save_path = '/nfs/ghome/liv
         torch.save({'epoch': epoch, 'model_state_dict': net.state_dict(), 'optimizer_state_dict': optimizer.state_dict(), 'loss': loss, 'test_accuracy': test_accuracy}, save_path + model+'_individual_'+str(class1)+'vs'+str(class2)+'.pth')
     return test_accuracy
 
-def cifar100_joint_train(model, class1a, class1b, class2a, class2b, save_path = '/nfs/ghome/live/ajain/checkpoints/di_cifar100/baseline/', epochs=500, batch_size = 128, lr=0.01, momentum=0.9, weight_decay=0.0001, seed=42):
+def cifar100_joint_train(model, class1a, class1b, class2a, class2b, save_path = '/nfs/ghome/live/ajain/checkpoints/di_cifar100/baseline/', epochs=500, batch_size = 128, lr=0.01, momentum=0.9, weight_decay=0.0001, random_split=True, seed=42, split_order = None, fine=False):
     assert model in ['lenet', 'resnet']
     assert class1a in range(1, 21)
     assert class1b in range(1, 21)
@@ -94,40 +82,26 @@ def cifar100_joint_train(model, class1a, class1b, class2a, class2b, save_path = 
     assert class2b in range(1, 21)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print(device)
+    assert device == 'cuda'
     print(torch.cuda.get_device_name())
     np.random.seed(seed)
     torch.manual_seed(seed)
 
     if model == 'lenet':
         net = models.LeNet5(2)
-        train_data = np.vstack([np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/train'+str(class1a)+'.npy'), np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/train'+str(class1b)+'.npy'), np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/train'+str(class2a)+'.npy'), np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/train'+str(class2b)+'.npy')]).mean(axis=1, keepdims=True)
-        train_labels = np.hstack([np.zeros(len(train_data)//2, dtype=int), np.ones(len(train_data)//2, dtype=int)])
-        test_data_a = np.vstack([np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/test'+str(class1a)+'.npy'), np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/test'+str(class2a)+'.npy')]).mean(axis=1, keepdims=True)
-        test_data_b = np.vstack([np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/test'+str(class1b)+'.npy'), np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/test'+str(class2b)+'.npy')]).mean(axis=1, keepdims=True)
-        test_labels = np.hstack([np.zeros(len(test_data_a)//2, dtype=int), np.ones(len(test_data_a)//2, dtype=int)])
+        train_data, train_labels, test_data_a, test_data_b, test_labels = utils.load_cifar100_4_classes(class1a, class1b, class2a, class2b, gray=True, shuffle=True, random_split=random_split, split_order=split_order, seed=seed, fine=fine)
         print('data and lenet loaded')
     elif model == 'resnet':
         net = models.resnet18vw(width=32)
         net.fc = nn.Linear(256, 2)
-        train_data = np.vstack([np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/train'+str(class1a)+'.npy'), np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/train'+str(class1b)+'.npy'), np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/train'+str(class2a)+'.npy'), np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/train'+str(class2b)+'.npy')])
-        train_labels = np.hstack([np.zeros(len(train_data)//2, dtype=int), np.ones(len(train_data)//2, dtype=int)])
-        test_data_a = np.vstack([np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/test'+str(class1a)+'.npy'), np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/test'+str(class2a)+'.npy')])
-        test_data_b = np.vstack([np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/test'+str(class1b)+'.npy'), np.load('/nfs/ghome/live/ajain/datasets/cifar100/class_wise/test'+str(class2b)+'.npy')])
-        test_labels = np.hstack([np.zeros(len(test_data_a)//2, dtype=int), np.ones(len(test_data_a)//2, dtype=int)])
+        train_data, train_labels, test_data_a, test_data_b, test_labels = utils.load_cifar100_4_classes(class1a, class1b, class2a, class2b, gray=False, shuffle=True, random_split=random_split, split_order=split_order, seed=seed, fine=fine)
         print('data and resnet loaded')
-    
 
     net = net.to(device)
     optimizer = torch.optim.SGD(net.parameters(), lr=lr, momentum=momentum, weight_decay=weight_decay)
     loss_fn = nn.CrossEntropyLoss()
-    print(f'Class 1a : {class1a} Class 1b {class1b} Class 2a {class2a} Class 2b {class2b}')
-
-    shuffle = np.random.permutation(len(train_data))
-    train_data = train_data[shuffle]
-    train_labels = train_labels[shuffle]
-
     batches = len(train_data)//batch_size
+    print(f'Class 1a : {class1a} Class 1b {class1b} Class 2a {class2a} Class 2b {class2b}')
 
     for epoch in range(epochs):
         for batch in range(batches-1):
@@ -170,11 +144,33 @@ def cifar100_joint_train(model, class1a, class1b, class2a, class2b, save_path = 
         torch.save({'epoch': epoch, 'model_state_dict': net.state_dict(), 'optimizer_state_dict': optimizer.state_dict(), 'loss': loss, 'test_accuracy_a': test_accuracy_a, 'test_accuracy_b':test_accuracy_b}, save_path + model+'_joint_'+str(class1a) + '_' + str(class1b)+'vs'+ str(class2a) + '_' + str(class2b) +'.pth')
     return test_accuracy_a, test_accuracy_b
 
+def disparate_impact(model, class1a, class1b, class2a, class2b, cross=False, seed=42, save_path = '/nfs/ghome/live/ajain/checkpoints/di_cifar100/baseline/', epochs=500, batch_size = 128, lr=0.01, momentum=0.9, weight_decay=0.0001):
+    print('Disparate Impact Analysis')
+    print('Model: ', model)
+    print('Classes: ', class1a, class1b, class2a, class2b)
+    assert model in ['lenet', 'resnet']
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    split_order = np.random.permutation(3000)
+    individual_accuracy_a = cifar100_individual_train(model, class1a, class2a, seed=seed, save_path=save_path, epochs=epochs, batch_size=batch_size, lr=lr, momentum=momentum, weight_decay=weight_decay, split_order=split_order)
+    individual_accuracy_b = cifar100_individual_train(model, class1b, class2b, seed=seed, save_path=save_path, epochs=epochs, batch_size=batch_size, lr=lr, momentum=momentum, weight_decay=weight_decay, split_order=split_order)
+    joint_accuracy_a, joint_accuracy_b = cifar100_joint_train(model, class1a, class1b, class2a, class2b, seed=seed, save_path=save_path, epochs=epochs, batch_size=batch_size, lr=lr, momentum=momentum, weight_decay=weight_decay, split_order=split_order)
+    disparate_impact = (joint_accuracy_a - joint_accuracy_b)/(individual_accuracy_a - individual_accuracy_b)
+    if cross:
+        cross_accuracy_a = cifar100_individual_train(model, class1a, class2b, seed=seed, save_path=save_path, epochs=epochs, batch_size=batch_size, lr=lr, momentum=momentum, weight_decay=weight_decay, split_order=split_order)
+        cross_accuracy_b = cifar100_individual_train(model, class1b, class2a, seed=seed, save_path=save_path, epochs=epochs, batch_size=batch_size, lr=lr, momentum=momentum, weight_decay=weight_decay, split_order=split_order)
+        print('Disparate Impact: {}, Individual Accuracy A: {}, Individual Accuracy B: {}, Joint Accuracy A: {}, Joint Accuracy B: {}, Cross Accuracy A: {}, Cross Accuracy B: {}'.format(disparate_impact, individual_accuracy_a, individual_accuracy_b, joint_accuracy_a, joint_accuracy_b, cross_accuracy_a, cross_accuracy_b))
+        return disparate_impact, individual_accuracy_a, individual_accuracy_b, joint_accuracy_a, joint_accuracy_b, cross_accuracy_a, cross_accuracy_b
+    else:
+        print('Disparate Impact: {}, Individual Accuracy A: {}, Individual Accuracy B: {}, Joint Accuracy A: {}, Joint Accuracy B: {}'.format(disparate_impact, individual_accuracy_a, individual_accuracy_b, joint_accuracy_a, joint_accuracy_b))
+        return disparate_impact, individual_accuracy_a, individual_accuracy_b, joint_accuracy_a, joint_accuracy_b
+
 # aquatic - 1, flower - 3
 # medium mammals - 13, large carnivores - 9
-cifar100_joint_train('resnet', 1, 9, 3, 13)
-cifar100_joint_train('lenet', 1, 9, 3, 13)
-cifar100_individual_train('resnet', 1, 3)
-cifar100_individual_train('lenet', 1, 3)
-cifar100_individual_train('resnet', 9, 13)
-cifar100_individual_train('lenet', 9, 13)
+#cifar100_joint_train('resnet', 1, 9, 3, 13)
+#cifar100_joint_train('lenet', 1, 9, 3, 13)
+#cifar100_individual_train('resnet', 1, 3)
+#cifar100_individual_train('lenet', 1, 3)
+#cifar100_individual_train('resnet', 9, 13)
+#cifar100_individual_train('lenet', 9, 13)
+disparate_impact('lenet', 1, 9, 3, 13, seed=42)
